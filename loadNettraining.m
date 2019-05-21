a646692cb5136e5000609ee85436afcc85435aa1
Harshit Gupta <harshit.gupta@epfl.ch>
2019-05-21 13:14:47 +0200

    Merge issues correctes

--- loadNettraining.m
+++ loadNettraining.m
@@ -1,10 +1,5 @@
 function Addresses=loadNettraining(param)
-<<<<<<< HEAD
-Stage=param.Stage;
-currentFolder = pwd;
 
-Addresses.export = fullfile('./training_result',['x' num2str(param.DownsamplingFactor),...
-=======
 
 %This function outputs the address to store training data and the address
 %to load a trained net to initiala  given stage
@@ -12,7 +7,6 @@ Stage=param.Stage;
 currentFolder = pwd;
 
 Addresses.export = fullfile('./testing_result',['x' num2str(param.DownsamplingFactor),...
->>>>>>> 8fe5c2fb1cbd72a1535299381ed04439ef1334f9
     '_',num2str(param.NoiseLevel),'dB','_Stage',num2str(param.Stage)],num2str(date) );  
 
 if Stage==1
@@ -23,10 +17,9 @@ if Stage==1
 elseif  Stage==2
     Addresses.loadnet= '';
     if  isempty(Addresses.loadnet)
-<<<<<<< HEAD
-=======
+
         % load the last modified/saved net from the previous stage.
->>>>>>> 8fe5c2fb1cbd72a1535299381ed04439ef1334f9
+
         Addresses.loadnet=LastModifiedNet(param);
     end
         
